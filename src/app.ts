2fade8db2f6ea615cf7f167c67ec03684d2c4a17
Marcello Andrade <marcellohfandrade@gmail.com>
2021-03-03 18:25:40 -0300

    remove post office route

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,6 @@ import "express-async-errors";
 import { doctorRoute } from "./routes/doctorRoute"
 import { specializationRoute } from "./routes/specializationRoute"
 import { doctorSpecializationRoute } from "./routes/doctorSpecializationRoute";
-import { postOfficeRoute } from "./routes/postOfficeRoute"
 import { ServiceError } from "./service/exception/ServiceError";
 
 createConnection();
@@ -17,7 +16,6 @@ app.use(express.json());
 app.use(doctorRoute);
 app.use(specializationRoute);
 app.use(doctorSpecializationRoute);
-app.use(postOfficeRoute);
 
 app.use(
     (error: Error, request: Request, response: Response, _next: NextFunction) => {
--- src/routes/postOfficeRoute.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const postOfficeRoute = Router();
-
-postOfficeRoute.get("/post-office/:cep", async (request, response) => {
-    const cep: string = request.params.cep;        
-    return response.status(200).json(cep);     
-})
-export { postOfficeRoute };
